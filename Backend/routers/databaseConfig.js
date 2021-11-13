aaa0755eadd22fab6842867a90db71ec3f51b521
ralphgerard <ralphramirez.20@ichat.sp.edu.sg>
2021-11-13 16:35:33 +0800

    Added project files

---
+++ Backend/routers/databaseConfig.js
@@ -0,0 +1,10 @@
+const pg = require("pg");
+
+const connection = new pg.Pool({
+  user: "uzlfkabz",
+  password: "ikVcW8DJbfrK_FZKQWSEe4wvPv77Jbfl",
+  host: "john.db.elephantsql.com",
+  database: "uzlfkabz"
+});
+
+module.exports = connection;
---
+++ Backend/routers/users.js
@@ -0,0 +1,2 @@
+const router = require("express").Router();
+const connection = require("../database.js");
