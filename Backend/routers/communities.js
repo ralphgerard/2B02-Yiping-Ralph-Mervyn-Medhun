8c46c03866200d67b65321a07815f0824e99e39c
ralphgerard <ralphramirez.20@ichat.sp.edu.sg>
2021-11-13 17:40:55 +0800

    Made create community in the backend

--- Backend/routers/communities.js
+++ Backend/routers/communities.js
@@ -1,4 +1,31 @@
 const router = require("express").Router();
 const connection = require("../database.js");
 
+//Create Community
+router.post('/createCommunity', (req, res) => {
+    
+    var communityName = req.body.name;
+    var communityImage = req.body.image;
+    var nsfwTag = req.body.nsfw;
+    var userid = req.body.userid
+    const createCommunityQuery = `
+    INSERT INTO communities(community_name, community_image , NSFW_community, userid)
+    VALUES ($1, $2, $3, $4);
+        `;
+  
+    connection.query(createCommunityQuery, [communityName, communityImage, nsfwTag, userid], (error, results) => {
+      if (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Error while creating community' });
+      } else {
+        console.log(results);
+        if (results.rowCount === 1) {
+          res.status(200).json({ message: 'Successfully created community' });
+        } else {
+          res.status(404).json({ error: `Unable to create community ${communityName}` });
+        }
+      }
+    });
+  });
+
 module.exports = router;
